8eafde1a4ccddbe83247f027c734080cff5628ba
Matthew Coburn <mattcoburn24@gmail.com>
2020-11-25 07:57:35 -0800

    added more elements and set up state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = React.useState(" ");
   return (
     <div className="App">
       <header>
-        <h1>Matt's to do list</h1>
+        <h1>Matt's to do list {inputText}</h1>
       </header>
-      <h1>hello react</h1>
+      <Form setInputText={setInputText} />
+      <TodoList inputText={inputText} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Form = ({ setInputText }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <form>
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <button className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
