f76a4e7815f66fcc66b5c27a01bc1147a49a5a63
Matthew Coburn <mattcoburn24@gmail.com>
2020-11-25 06:23:23 -0800

    set up dev env

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Matt's to do list</h1>
+      </header>
+      <h1>hello react</h1>
+    </div>
+  );
+}
+
+export default App;
